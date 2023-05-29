2ee00ac98a56ec1eda09b022d308671cca222f48
Colten <likeyou600@gmail.com>
2023-05-29 19:01:03 +0800

    Feat : 添加基本library

--- src/main.js
+++ src/main.js
@@ -2,4 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from "element-plus";
+import locale from "element-plus/lib/locale/lang/zh-tw";
+import "element-plus/dist/index.css";
+import "element-plus/theme-chalk/display.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap"
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
+const app = createApp(App);
+
+app.use(ElementPlus, {
+    locale: { locale },
+});
+
+app.use(router).mount('#app')
